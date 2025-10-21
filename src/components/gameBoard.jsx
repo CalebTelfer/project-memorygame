1a139ff03b2c4609b67ac32c3df853f773042fb3
caleb <calebtelfe@gmail.com>
2025-10-21 15:16:38 -0400

    fetching pokemon data

--- src/components/gameBoard.jsx
+++ src/components/gameBoard.jsx
@@ -3,6 +3,39 @@ import MemoryCard from "./memoryCard"
 import '../componentCSS/gameBoard.css'
 
 function GameBoard({setScore}) {
+    const pokemonNames = ["Ditto", "Pikachu", "Charizard", "Blastoise", "Venasaur", "Gengar", "Zacian", "Lugia", "Victini", "Articuno"];
+    const [pokemonList, setList] = useState([]);
+
+
+
+    useEffect(() => {
+
+        async function fetchPokemonData() {
+            const promises = pokemonNames.map(async (pokemon) => {
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+                return response.json();
+            });
+
+            try {
+                const results = await Promise.all(promises);
+
+                const formattedResults = results.map(result => ({
+                    name: result.name,
+                    img: result.sprites.other["official-artwork"].front_default
+                }));
+
+                setList(formattedResults);
+
+            } catch (error) {
+                console.log("One or more failed");
+            }
+        }
+
+        fetchPokemonData();
+    }, [])
+
+
+
     const handleCardClick = () => {
         setScore(prevScore => prevScore + 1); // update score
       };
